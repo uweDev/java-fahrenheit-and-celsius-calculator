cddf4afc8acd4a2e313eb701db69ce9ff9a3c55e
Uwe Hellmann <58147568+uweDev@users.noreply.github.com>
2019-12-16 13:30:53 +0100

    added comments

--- temperaturApp.java
+++ temperaturApp.java
@@ -1,3 +1,9 @@
+/*
+This is a Open-Source-Software.
+This open source software was released on 16.12.2019 by Uwe Hellmann and is subject to the MIT License.
+You can download this Open-Source-Software from here: https://github.com/uweDev/java-fahrenheit-and-celsius-calculator
+*/
+
 package temperaturApp;
 
 import java.awt.BorderLayout;
@@ -44,6 +50,8 @@ public class temperaturApp extends JFrame {
 	 * Create the frame.
 	 */
 	public temperaturApp() {
+		
+		// Configure the window
 		setTitle("Fahrenheit Celsius Rechner");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 277);
@@ -53,44 +61,59 @@ public class temperaturApp extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+		// Set fahrenheit label
 		JLabel fahrenheitLabel = new JLabel("Grad Fahrenheit:");
 		fahrenheitLabel.setBounds(10, 11, 113, 19);
 		contentPane.add(fahrenheitLabel);
 		
+		// Set fahrenheit text field
 		fahrenheit = new JTextField();
 		fahrenheit.setBounds(10, 29, 188, 32);
 		contentPane.add(fahrenheit);
 		fahrenheit.setColumns(10);
 		
+		// Set celsius label
 		JLabel celsiusLabel = new JLabel("Grad Celsius:");
 		celsiusLabel.setBounds(10, 90, 188, 19);
 		contentPane.add(celsiusLabel);
 		
+		// Set celsius text field
 		celsius = new JTextField();
 		celsius.setBounds(10, 108, 188, 32);
 		contentPane.add(celsius);
 		celsius.setColumns(10);
 		
+		// Set convert button 
 		btnConvert = new JButton("Umrechnen");
 		btnConvert.setForeground(Color.WHITE);
 		btnConvert.setBackground(new Color(0, 102, 153));
 		btnConvert.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				// Set the data types
+				double fahrenheitResult, celsiusResult;
+				
 				if (!fahrenheit.getText().equals("")){
-					double fahrenheitResult, celsiusResult;
+					// Process text
 					fahrenheitResult = Double.parseDouble(fahrenheit.getText());
+					// Calculate the result
 					celsiusResult = (fahrenheitResult - 32) * 5 / 9;
+					// Show result
 					celsius.setText(String.valueOf(celsiusResult));
+					// Show formula
 					formel.setText(("Formel: (" + fahrenheitResult + "\u00B0" + "F-32)+5/9=" + celsiusResult + "\u00B0" + "C"));
 				}
 				else if (!celsius.getText().equals("")){
-					double fahrenheitResult, celsiusResult;
+					// Process text
 					celsiusResult = Double.parseDouble(celsius.getText());
+					// Calculate the result
 					fahrenheitResult = (celsiusResult * 9 / 5) + 32;
+					// Show result
 					fahrenheit.setText(String.valueOf(fahrenheitResult));
+					// Show formula
 					formel.setText(("Formel: (" + celsiusResult + "\u00B0" + "C*9/5)+32=" + fahrenheitResult + "\u00B0" + "F"));
 				}
 				else {
+					// Show the error message
 					JOptionPane.showMessageDialog(
 					null, "Bitte geben Sie einen Wert an!");
 				}
@@ -99,6 +122,7 @@ public class temperaturApp extends JFrame {
 		btnConvert.setBounds(236, 200, 188, 32);
 		contentPane.add(btnConvert);
 		
+		// Set formula
 		formel = new JLabel("");
 		formel.setBounds(10, 200, 216, 32);
 		contentPane.add(formel);
@@ -106,10 +130,12 @@ public class temperaturApp extends JFrame {
 		JButton btnEmpty = new JButton("Textfelder leeren");
 		btnEmpty.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				// Clear the text fields
 				fahrenheit.setText("");
 				celsius.setText("");
 			}
 		});
+		// Set reset button
 		btnEmpty.setForeground(Color.WHITE);
 		btnEmpty.setBackground(new Color(0, 153, 204));
 		btnEmpty.setBounds(236, 157, 188, 32);
